40c51fd97ad46ca7060570bc7249a402dd7cacd5
github <babwitforums@gmail.com>
2016-03-07 17:34:21 +0530

    initail commit

---
+++ src/main/java/icici/paymentmodule/App.java
@@ -0,0 +1,13 @@
+package icici.paymentmodule;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
